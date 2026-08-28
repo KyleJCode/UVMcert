840f74d4d93085239ef2ee58c4460804b03ff178
KyleJCode <kylestyle05@gmail.com>
2026-08-27 21:10:36 -0400

    lab1 - Needs Testing

--- uvma_training_1.2.6rev1/uvm/lab01_data/sv/yapp_packet.sv
+++ uvma_training_1.2.6rev1/uvm/lab01_data/sv/yapp_packet.sv
@@ -7,20 +7,63 @@ Copyright Cadence Design Systems (c)2015
 -----------------------------------------------------------------*/
 
 // Define your enumerated type(s) here
+typedef enum {GOOD_PARITY, BAD_PARITY} parity_t;
 
 class yapp_packet extends uvm_sequence_item;
 
 // Follow the lab instructions to create the packet.
 // Place the packet declarations in the following order:
 
-  // Define protocol data
+  // Define protocol data //
+  // Packet Specs
+  rand bit [5:0] length;
+  rand bit [1:0] addr;
+  rand bit [7:0] payload[];
+  bit [7:0] parity; 
 
-  // Define control knobs
+  // Define control knobs //
+  rand int packet_delay;
+  rand parity_t parity_type;
 
-  // Enable automation of the packet's fields
+  // Enable automation of the packet's fields //
+  `uvm_object_utils_begin(yapp_packet)
+    `uvm_field_int(length,       UVM_ALL_ON)
+    `uvm_field_int(addr,         UVM_ALL_ON)
+    `uvm_field_array_int(payload, UVM_ALL_ON)
+    `uvm_field_int(parity,      UVM_ALL_ON)
+    `uvm_field_enum(parity_t, parity_type, UVM_ALL_ON)
+    `uvm_field_int(packet_delay, UVM_ALL_ON | UVM_DEC | UVM_NOCOMPARE)
+  `uvm_object_utils_end
 
-  // Define packet constraints
+  // Define packet constraints // 
+  constraint default_length { length > 0; length < 64; }
+  constraint payload_size   { length == payload.size(); }
+  constraint default_delay  { packet_delay >= 0; packet_delay < 20; }
+  constraint default_parity { parity_type dist {BAD_PARITY := 1, GOOD_PARITY := 5}; }
+  constraint default_addr  { addr != 2'b11; }
 
-  // Add methods for parity calculation and class construction
+  // Add methods for parity calculation and class construction //
+  // Parity Setting/Calculating
+  function bit [7:0] calc_parity();
+    calc_parity = {length, addr};
+    foreach (payload [i])
+      calc_parity = calc_parity ^ payload[i];
+  endfunction : calc_parity
 
+  // sets parity field according to parity_type
+  function void set_parity();
+    parity = calc_parity();
+    if (parity_type == BAD_PARITY)
+      parity++;
+  endfunction : set_parity
+
+  function void post_randomize();
+    set_parity();
+  endfunction : post_randomize
+
+  // Class Constructor
+  function new(string name = "yapp_packet");  
+    super.new(name);
+  endfunction : new
+  
 endclass: yapp_packet
--- uvma_training_1.2.6rev1/uvm/lab01_data/tb/top.sv
+++ uvma_training_1.2.6rev1/uvm/lab01_data/tb/top.sv
@@ -8,12 +8,36 @@ Copyright Cadence Design Systems (c)2015
 
 module top;
 // import the UVM library
+import uvm_pkg::*;
+
 // include the UVM macros
+`include "uvm_macros.svh"
 
 // import the YAPP package
+import yapp_pkg::*;
 
 // generate 5 random packets and use the print method
 // to display the results
+
+yapp_packet my_packet;
+yapp_packet copy_packet;
+yapp_packet clone_packet;
+uvm_table_printer custom = new();
+
+  string name;
+  int ok;
+
+  initial begin
+  // construct the packet for copy
+  copy_packet = new("copy_packet");
+
+  for (int i=0; i<5; i++) begin
+    // allocate each packet
+    packet = new($sformatf("packet%0d",i));
+    ok = packet.randomize();
+    packet.print();
+  end
+end
 
 // experiment with the copy, clone and compare UVM method
 endmodule : top
