7749f807b7db3be9bd6e0fb1b03c73859e6dc827
KyleJCode <kylestyle05@gmail.com>
2026-09-14 19:13:44 -0400

    Initial Commit, files for lab05

---
+++ uvma_training_1.2.6rev1/uvm/lab05_seq/sv/yapp_pkg.sv
@@ -0,0 +1,12 @@
+package yapp_pkg;
+
+  import uvm_pkg::*;
+  `include "yapp_packet.sv"
+  `include "yapp_tx_monitor.sv"
+  `include "yapp_tx_sequencer.sv"
+  `include "yapp_tx_seqs.sv"
+  `include "yapp_tx_driver.sv"
+  `include "yapp_tx_agent.sv"
+  `include "yapp_env.sv" 
+
+endpackage : yapp_pkg
---
+++ uvma_training_1.2.6rev1/uvm/lab05_seq/tb/run.f
@@ -0,0 +1,17 @@
+// Run file for UVM setup
+-64
+-uvmhome $UVMHOME
+-incdir ../sv
+
++UVM_VERBOSITY=UVM_LOW
+
+//+UVM_TESTNAME=base_test
++UVM_TESTNAME=short_packet_test
+//+UVM_TESTNAME=set_config_test
+//+UVM_TESTNAME=test2
+
+//+SVSEED=random
+
+../sv/yapp_pkg.sv
+
+top.sv
